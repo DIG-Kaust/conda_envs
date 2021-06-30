0d9472a874c61551737fbafa6c14df93c74f0c3e
Arturo R Sanchez <sanchear@kw60986.KAUST.EDU.SA>
2021-06-30 09:58:28 +0300

    Added pytorch env

---
+++ install_pytorch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 
+# Installer for PyTorch GPU environment with Pytorch+Cupy+PyTorch and CUDA 10.2
+# 
+# Run: ./install_pytorch.sh
+# 
+# M. Ravasi, 03/02/2021
+
+echo 'Creating PyTorch environment with Pytorch and CUDA 10.2'
+
+# create conda env
+conda env create -f environment_pytorch.yml
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pytorch_gpu
+conda env list
+echo 'Created and activated environment:' $(which python)
+
+# check cupy works as expected
+echo 'Checking cupy version and running a command...'
+python -c 'import torch; print(torch.__version__);  print(torch.cuda.get_device_name(torch.cuda.current_device())); print(torch.ones(10).to("cuda"))'
+
+echo 'Done!'
+
